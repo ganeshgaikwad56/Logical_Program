ec696db9d6f26e47676998ec49b2957f0c44c156
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-02-23 23:47:25 +0530

    Adding Reverse number program file

---
+++ ReverseNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Programing
+{
+    public class ReverseNumber
+    {
+        public static void ReverseThenNumber()
+        {
+            int n, reverse = 0, rem;
+            Console.WriteLine("Enter a number");
+            n=Convert.ToInt32(Console.ReadLine());
+
+            while(n!=0)
+            {
+                rem = n % 10;
+                reverse = reverse * 10 + rem;
+                n/=10;
+            }
+            Console.WriteLine("Reverse number " + reverse);
+        }
+    }
+}
