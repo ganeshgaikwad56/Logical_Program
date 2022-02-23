3ef33d6d5ddfda6fcc6fafb643447166d4c17ec8
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-02-23 23:42:16 +0530

    pushing program.cs file

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Logical_Programing
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //PerfectNumber.FindPerfectNumber();
+            //PrimeNumber.CheckPrimeNumber();
+            // FibonacciSeries.Fibonacci();
+            ReverseNumber.ReverseThenNumber();
+        }
+    }
+}
