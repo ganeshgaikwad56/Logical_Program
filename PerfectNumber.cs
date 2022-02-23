c82d787533cb642d32d3532b08e5b9ae2596bca1
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-02-23 23:45:42 +0530

    Adding perfect number program file

---
+++ PerfectNumber.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Programing
+{
+    public class PerfectNumber
+    {
+        public static void FindPerfectNumber()
+        {
+            int n, i, sum;
+            int mn, mx;
+
+            Console.WriteLine("Find the perfect number within a given range");
+            Console.WriteLine("-------------");
+            Console.WriteLine("Enter the starting number");
+            mn=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the ending number");
+            mx=Convert.ToInt32(Console.ReadLine());
+            for (n=mn; n<=mx; n++)
+            {
+                i = 1;
+                sum = 0;
+                while(i<n)
+                {
+                    if (n % i == 0)
+                        sum = sum + i;
+                    i++;
+                }
+                if (sum == n)
+                    Console.Write(" {0} ", n);
+            }
+        }
+    }
+}
